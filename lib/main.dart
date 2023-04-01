b60d8cf0a56205a1937a17f5ac3e67826ffe7ea3
Ranjith Menon <60739917+theranjithmenon@users.noreply.github.com>
2023-04-01 16:18:01 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:network_ckeck/controller/number_controller.dart';
 import 'dependancy_injection.dart';
 
 Future<void> main() async {
@@ -16,43 +15,18 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key, required this.title});
-  final String title;
-  final NumberController numberController = Get.put(NumberController());
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Obx(
-              () => Text(
-                '${numberController.count.value}',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: numberController.increment(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return const Scaffold(
+      body: Center(child: Text('Home Page')),
     );
   }
 }
